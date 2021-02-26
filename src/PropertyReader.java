51a018b32b28d9347ef597e407740ec415a888d1
Андреев Иван Григорьевич <69AndreevIG@region.cbr.ru>
2021-02-26 11:56:25 +0700

    res

--- src/PropertyReader.java
+++ src/PropertyReader.java
@@ -1,12 +1,14 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class PropertyReader {
 
-    public static String INPUT_DIRECTORY;
-    public static String OUTPUT_DIRECTORY;
+    public static Path INPUT_DIRECTORY;
+    public static Path OUTPUT_DIRECTORY;
     public static int INITIAL_DELAY;
     public static int PERIOD;
 
@@ -15,8 +17,8 @@ public class PropertyReader {
         String pathPropFile = "resources.properties";
         try ( FileInputStream fileInputStream = new FileInputStream(new File(pathPropFile)) ) {
             properties.load(fileInputStream);
-            INPUT_DIRECTORY = properties.getProperty("INPUT_DIRECTORY");
-            OUTPUT_DIRECTORY = properties.getProperty("OUTPUT_DIRECTORY");
+            INPUT_DIRECTORY = Paths.get(properties.getProperty("INPUT_DIRECTORY"));
+            OUTPUT_DIRECTORY = Paths.get(properties.getProperty("OUTPUT_DIRECTORY"));
             INITIAL_DELAY = Integer.parseInt(properties.getProperty("INITIAL_DELAY"));
             PERIOD = Integer.parseInt(properties.getProperty("PERIOD"));
         }
--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,26 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.*;
-
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Test {
 
-    private void exec() {
-        File inputFolder = new File(PropertyReader.INPUT_DIRECTORY);
-        File[] listOfFiles = inputFolder.listFiles();
-        if (listOfFiles != null)
+    private void exec()  {
+        List<File> listOfFiles = null;
+        try {
+            listOfFiles = Files.walk(PropertyReader.INPUT_DIRECTORY).filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (listOfFiles.size() != 0)
             for (File file : listOfFiles){
                 String fileName = file.getName();
-                try(FileInputStream fileInputStream = new FileInputStream(file);
+                try(FileInputStream fileInputStream = new FileInputStream(PropertyReader.INPUT_DIRECTORY + "/" + fileName);
                     FileOutputStream fileOutputStream = new FileOutputStream(PropertyReader.OUTPUT_DIRECTORY + "/" +fileName)){
                     copy(fileInputStream, fileOutputStream);
                 } catch (IOException e){
