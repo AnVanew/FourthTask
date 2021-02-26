f82bfd3d94bd79e138d502df9265b114119acae6
Андреев Иван Григорьевич <69AndreevIG@region.cbr.ru>
2021-02-26 13:16:30 +0700

    res

--- src/Test.java
+++ src/Test.java
@@ -1,40 +1,37 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 public class Test {
 
-    private void exec()  {
+    private void moveFilesFromDir()  {
         try {
-            List<File> listOfFiles = null;
-            listOfFiles = Files.walk(PropertyReader.INPUT_DIRECTORY).filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList());
+            List<Path> listOfFiles = Files.walk(PropertyReader.INPUT_DIRECTORY).filter(Files::isRegularFile).collect(Collectors.toList());
             if (listOfFiles.size() != 0)
-                for (File file : listOfFiles){
-                   copyAndDeleteFile(file);
+                for (Path file : listOfFiles){
+                    copyAndDeleteFile(file);
                 }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void copyAndDeleteFile (File file) throws IOException {
-        String fileName = file.getName();
-        try(FileInputStream fileInputStream = new FileInputStream(PropertyReader.INPUT_DIRECTORY + "/" + fileName);
-            FileOutputStream fileOutputStream = new FileOutputStream(PropertyReader.OUTPUT_DIRECTORY + "/" +fileName)){
-            copy(fileInputStream, fileOutputStream);
-        }
-        file.delete();
+    private void copyAndDeleteFile (Path file) throws IOException {
+        copyFile(file);
+        Files.delete(file);
     }
 
-    private void start() {
-        ScheduledExecutorService s = Executors.newSingleThreadScheduledExecutor();
-        s.scheduleAtFixedRate(this::exec, PropertyReader.INITIAL_DELAY, PropertyReader.PERIOD, TimeUnit.SECONDS);
+    private void copyFile(Path file) throws IOException{
+        Path fileName = file.getFileName();
+        try(FileInputStream fileInputStream = new FileInputStream(PropertyReader.INPUT_DIRECTORY + "/" + fileName);
+            FileOutputStream fileOutputStream = new FileOutputStream(PropertyReader.OUTPUT_DIRECTORY + "/" +fileName))
+        {
+            copy(fileInputStream, fileOutputStream);
+        }
     }
 
     private void copy(InputStream source, OutputStream target) throws IOException {
@@ -43,6 +40,11 @@ public class Test {
         while ((length = source.read(buf)) > 0) {
             target.write(buf, 0, length);
         }
+    }
+    
+    private void start() {
+        ScheduledExecutorService s = Executors.newSingleThreadScheduledExecutor();
+        s.scheduleAtFixedRate(this::moveFilesFromDir, PropertyReader.INITIAL_DELAY, PropertyReader.PERIOD, TimeUnit.SECONDS);
     }
 
     public static void main(String[] args) {
